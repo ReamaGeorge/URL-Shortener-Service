c469f5959445dde4e474bb503148817f2481694b
ReamaGeorge <georgereama@yahoo.com>
2022-06-30 15:59:25 +0100

    First commit

---
+++ I4G0015401F9/links/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views 
+
+app_name="link"
+
+urlpatterns = [
+    path("create/", views.PostCreateAPI.as_view(), name="api_create"),
+    path("update/<int:pk>", views.PostUpdateAPI.as_view(), name="api_update"),
+    path("delete/<int:pk>", views.PostDeleteAPI.as_view(), name="api_delete"),
+    path("", views.PostListAPI.as_view(), name="api_list"),
+]
---
+++ I4G0015401F9/links/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Link
+from .serializers import LinkSerializer
+
+# Create your views here.
+class PostListAPI(generics.ListAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class PostCreateAPI(generics.CreateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostDetailAPI(generics.RetrieveAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostUpdateAPI(generics.UpdateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class PostDeleteAPI(generics.DestroyAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
